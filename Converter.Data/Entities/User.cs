c6ecdb9c83f76090f799a95fa759a5a4e05c1a48
Asrorbek <as.abrorov@gmail.com>
2024-03-10 20:00:48 +0500

    Initial Commit

---
+++ Converter.Data/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Data.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? UserName { get; set; }
+    public long TelegramUserId { get; set; }
+    public string Email { get; set; }
+}
--- Converter/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Converter;
-
-public class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
