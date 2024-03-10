1e2ede1198fb2bd637e034cfa8150d428a641dd3
Asrorbek <as.abrorov@gmail.com>
2024-03-10 22:56:31 +0500

    Created Folders and added referebces

---
+++ Converter.Services/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Converter.Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
